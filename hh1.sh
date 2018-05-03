7db8b39d46cd4afc6562335a1461aa5463c78939
huangxiaosong1 <huangxiaosong@caicloud.io>
2018-05-03 14:24:27 +0800

    add .
    Signed-off-by: huangxiaosong1 <huangxiaosong@caicloud.io>

---
+++ hh1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+i=1
+while [ $i -le 10000000 ]
+do
+	echo "hellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohello"
+	i=$(($i+1))
+done
